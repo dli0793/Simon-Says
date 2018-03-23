9284e4a4c00a15dbdf3eb40da3e8defd9619434f
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2018-03-23 09:22:12 -0400

    :)

---
+++ Simon Says/src/GameCode.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class GameCode extends Application
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage priaryStage) throws Exception 
+	{
+		primaryStage.setTitle("My First JavaFX App");
+		primaryStage.show();
+	}
+
+}
