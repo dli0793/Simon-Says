67d9ed72fa2b2232915d9d24955b604ea0067386
lid <lid@CE031926752453.conedison.net>
2018-03-27 19:58:18 -0400

    Attempting to make buttons

--- Simon Says/src/GameCode.java
+++ Simon Says/src/GameCode.java
@@ -1,9 +1,17 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
-import javafx.scene.control.Button;
 
+import java.util.ArrayList;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.SequentialTransition;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class GameCode extends Application
 {
@@ -11,12 +19,28 @@ public class GameCode extends Application
 	public static void main(String[] args) 
 	{
 		Application.launch(args);
+		
 	}
+	
+		@Override
+		public void start(Stage arg0) throws Exception {
+		
+		    }
+	
+		
+	
 
-	@Override
-	public void start(Stage primaryStage) throws Exception 
-	{
-		//Labels 
+	//*/private Scene scene1, scene2;
+   // private Button button2_1, button1_2, blue, red, yellow, green;
+   // private Label label1, label2;
+   // private VBox layout1, layout2;
+    //private Stage frame;
+
+
+	//@Override
+	//public void start(Stage primaryStage) throws Exception 
+	//{
+		/*/Labels 
 		primaryStage.setTitle("Simon Says");
 		Label label = new Label("Welcome to Simon Says! Follow the light patterns as displayed. Let's see how well your memory is!!");
         Scene scene = new Scene(label, 400, 500); //not showing up
@@ -30,7 +54,7 @@ public class GameCode extends Application
         //for making stuff pretty later on
         /*FileInputStream input = new FileInputStream("resources/images/iconmonstr-home-6-48.png");
         Image image = new Image(input);
-        ImageView imageView = new ImageView(image);*/
+        ImageView imageView = new ImageView(image);
 
         Button button = new Button("Green");
         Scene scene3 = new Scene(button, 50, 50);
@@ -47,6 +71,39 @@ public class GameCode extends Application
         button.setMinSize(10,10);
         button.setMaxSize(200,200);
         button.setPrefSize(50,50);
-	}
+		frame = primaryStage;
+
+        // Create buttons
+        button1_2 = new Button("Go to Main Menu");
+        button2_1 = new Button("Start Game");
+        blue = new Button("Blue");
+        red = new Button("Red");
+        yellow = new Button("Yellow");
+        green = new Button("Green");
+        
+        // Button action listener
+        button1_2.setOnAction(e -> frame.setScene(scene1));
+        button2_1.setOnAction(e -> frame.setScene(scene2));
+
+        // Create labels
+        label1 = new Label("This is the main menu. Press 'Start Game' to play.");
+        label2 = new Label("Follow the patterns that you see.");
+
+        // Create layouts
+        layout1 = new VBox(20);
+        layout2 = new VBox(20);
+
+        // Add elements to each layout
+        layout1.getChildren().addAll(label1, button2_1);
+        layout2.getChildren().addAll(label2, button1_2, blue, red, yellow, green);
+
+        // Create scenes
+        scene1 = new Scene(layout1, 400,400);
+        scene2 = new Scene(layout2, 400,400);
+
+        frame.setScene(scene1);
+        frame.setTitle("Simon Says");
+        frame.show();*/
+	//}
 
 }
